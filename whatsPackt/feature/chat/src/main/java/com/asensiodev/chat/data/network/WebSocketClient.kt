6f3f74fa349da6bf3f70b221b64120e27df839fe
asensiodev <asensiodev@gmail.com>
2024-09-14 12:48:27 +0200

    Add rest client

--- whatsPackt/feature/chat/src/main/java/com/asensiodev/chat/data/network/WebSocketClient.kt
+++ whatsPackt/feature/chat/src/main/java/com/asensiodev/chat/data/network/WebSocketClient.kt
@@ -2,14 +2,22 @@ package com.asensiodev.chat.data.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
-import kotlinx.serialization.json.Json
 
 object WebSocketClient {
     val client = HttpClient(OkHttp) {
         install(WebSockets) {
             contentConverter = KotlinxWebsocketSerializationConverter(Json)
+        }
+    }
+}
+
+object RestClient {
+    val client = HttpClient {
+        install(ContentNegotiation) {
+            json()
         }
     }
 }
